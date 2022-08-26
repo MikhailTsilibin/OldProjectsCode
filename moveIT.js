14124636d34ebd7aae4abcab80202a61d2f43ec5
Mikhail <112169268+MikhailTsilibin@users.noreply.github.com>
2022-08-26 14:37:31 +0300

    Add files via upload

---
+++ moveIT.js
@@ -0,0 +1,29 @@
+﻿internal var animator;
+var vertical : float;
+var horizontal : float;
+var jump : float;
+
+function Start(){
+	animator = GetComponent(Animator);
+}
+
+function FixedUpdate(){
+	animator.SetFloat("Speed", vertical, 0.1f, Time.deltaTime);
+	animator.SetFloat("Direction", vertical, 0.1f, Time.deltaTime);	
+	animator.SetFloat("Jump", jump);	
+	Jumping();
+}
+
+function Update(){
+	vertical = Input.GetAxis("Vertical");
+	horizontal = Input.GetAxis("Horizontal");
+}
+
+function Jumping(){
+	if(Input.GetKey(KeyCode.Space)){
+		jump = 1.0;
+}
+	else{
+		jump = 0.0;
+	}		
+}
